460fdf536b0d6ff6ef01e6f0a787594c989a0835
SunGj <sungj66@qq.com>
2021-03-22 17:39:38 +0800

    flask样例程序

---
+++ Flask/flasktest.py
@@ -0,0 +1,16 @@
+#coding=gbk
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', ])
+def index():
+    # return {
+    #     "msg": "success",
+    #     "data": "welcome to use flask."
+    # }
+    return 'hello word!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # '0.0.0.0'的目的是允许别的设备通过网络访问
