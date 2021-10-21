61a8674c3cb2bc8704a45c743845462f5e299bb9
SunGj <sungj66@qq.com>
2021-10-21 16:25:58 +0800

    comic download

--- dbm-downloader-master/dbmultiverse.py
+++ dbm-downloader-master/dbmultiverse.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 #! python3
 # Downloads all the english images/pages for Dragonball Multiverse.
 
@@ -9,38 +10,67 @@ __author__ = "Shafqat Dulal"
 __version__ = "1.0.0"
 
 url = "https://www.dragonball-multiverse.com/cn/chapters.html?comic=page"
-os.makedirs('dbm', exist_ok=True)
+print(os.getcwd())
+os.makedirs('/dbm', exist_ok=True)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/601.4.4 (KHTML, like Gecko) Version/9.0.3 Safari/601.4.4'}
+
+print("connectting....wait....")
 
 # Find the last page, then work from there.
 res = requests.get(url)
 res.raise_for_status()
 
-soup = bs4.BeautifulSoup(res.text)
+soup = bs4.BeautifulSoup(res.text, features="lxml")
 lastPageLink = soup.select('.chapters p a')[-1]
 print("There are currently " + lastPageLink.text + " pages.")
-url = "http://www.dragonball-multiverse.com/en/" + lastPageLink.get('href')
+
+
+StartPage = input("please input start page number:")
+
+EndPage = 'page-' + input("please input end page number:")
+
+url = "https://www.dragonball-multiverse.com/cn/page-" + StartPage+".html"
+
+print(url)
 
 lastPageFinished = False
+presentPage = int(StartPage)
+
 while lastPageFinished == False:
+
     res = requests.get(url)
     res.raise_for_status()
 
-    soup = bs4.BeautifulSoup(res.text)
-    imageElem = soup.select('#balloonsimg img')
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+    imageElem = soup.select('#balloonsimg')
     if (len(imageElem) != 0):
         imageUrl = 'http://www.dragonball-multiverse.com' + \
-            soup.select('#balloonsimg img')[0].get('src')
-        print('Downloading image %s...' % imageUrl)
-        res = requests.get(imageUrl)
+            imageElem[0].get('src')
+
+        print('Downloading image %s...,wait' % imageUrl)
+        res = requests.get(imageUrl, headers=headers)
         res.raise_for_status()
 
-        imageFile = open(os.path.join('dbm', os.path.basename(imageUrl)), 'wb')
+        imageFile = open(os.path.join(os.getcwd(),
+                                      '\dbm\\', os.path.basename(str(presentPage)+".jpg")), 'wb')
         for chunk in res.iter_content(100000):
             imageFile.write(chunk)
         imageFile.close()
 
-    if not ('page-0' in url):
-        prevLink = soup.select('a[rel="prev"]')[0]
-        url = "http://www.dragonball-multiverse.com" + prevLink.get('href')
+    else:
+        print("cannot find image file! please check url")
+        break
+
+    if not (EndPage in url):
+        nextLink = soup.select('a[rel="next"]')
+        if(len(nextLink) != 0):
+            url = "http://www.dragonball-multiverse.com" +
+            nextLink[0].get('href')
+            print(url)
+            presentPage = presentPage+1
+        else:
+            lastPageFinished = True
     else:
         lastPageFinished = True
